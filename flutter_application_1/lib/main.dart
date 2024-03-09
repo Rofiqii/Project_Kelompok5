ef5bf672911837535d403bfce19ef6e20524e39a
Nomikuza <uzumakisujai@gmail.com>
2024-03-09 23:25:20 +0700

    perf main for splash & sign up or sign in

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/auth/sign_up_page.dart';
+import 'package:flutter_application_1/controllers/popular_product_controller.dart';
 import 'package:flutter_application_1/pages/food/popular_food_detail.dart';
+import 'package:flutter_application_1/pages/food/recommended_food_detail.dart';
+import 'package:flutter_application_1/pages/home/food_page_body.dart';
 import 'package:flutter_application_1/pages/home/main_food_page.dart';
+import 'package:flutter_application_1/pages/splash/splash_page.dart';
+import 'package:flutter_application_1/routes/routes_helper.dart';
 import 'package:get/get.dart';
+import 'helper/dependencies.dart' as dep;
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
@@ -20,7 +29,9 @@ class MyApp extends StatelessWidget {
           // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           // useMaterial3: true,
           ),
-      home: PopularFoodDetail(),
+      home: SplashScreen(),
+      initialRoute: RouteHelper.getSplashPage(),
+      getPages: RouteHelper.routes,
     );
   }
 }
