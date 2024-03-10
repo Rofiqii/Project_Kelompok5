51ee8d73796d37474035ae6e46d09b5bf1258902
Nomikuza <uzumakisujai@gmail.com>
2024-03-10 16:44:14 +0700

    add: model for signup & link to website

---
+++ flutter_application_1/lib/utils/app_constants.dart
@@ -0,0 +1,13 @@
+class AppConstants {
+  static const String APP_NAME = "DBFood";
+  static const int APP_VERSION = 1;
+
+  static const String BASE_URL = "https:mvs.bslmeiyu.com";
+  static const String POPULAR_PRODUCT_URI = "/api/v1/products/popular";
+  static const String RECOMMENDED_PRODUCT_URI = "/api/v1/products/recommended";
+
+  //auth end points
+  static const String REGISTRATION_URI = "/api/v1/auth/register";
+
+  static const String TOKEN = "DBtoken";
+}
