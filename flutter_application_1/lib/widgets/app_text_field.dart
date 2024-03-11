b39d41b68963888e7401ff12c5886561635bb5fd
Nomikuza <uzumakisujai@gmail.com>
2024-03-12 00:08:13 +0700

    style: matching text field between sign in & sign up page

--- flutter_application_1/lib/widgets/app_text_field.dart
+++ flutter_application_1/lib/widgets/app_text_field.dart
@@ -7,11 +7,13 @@ class AppTextField extends StatelessWidget {
   final TextEditingController textController;
   final String hintText;
   final IconData icon;
-  const AppTextField(
+  bool isObscure;
+  AppTextField(
       {super.key,
       required this.textController,
       required this.hintText,
-      required this.icon});
+      required this.icon,
+      this.isObscure = false});
 
   @override
   Widget build(BuildContext context) {
@@ -23,12 +25,13 @@ class AppTextField extends StatelessWidget {
           borderRadius: BorderRadius.circular(Dimensions.radius30),
           boxShadow: [
             BoxShadow(
-                blurRadius: 10,
-                spreadRadius: 7,
-                offset: Offset(1, 10),
+                blurRadius: 3,
+                spreadRadius: 1,
+                offset: Offset(1, 1),
                 color: Colors.grey.withOpacity(0.2))
           ]),
       child: TextField(
+        obscureText: isObscure ? true : false,
         controller: textController,
         decoration: InputDecoration(
           //hintText,
