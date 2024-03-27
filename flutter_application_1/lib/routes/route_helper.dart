046a19adec8e3e4d5ff045b297668b6eca6b27ff
Nomikuza <uzumakisujai@gmail.com>
2024-03-27 10:19:51 +0700

    refactor: rename route_helper

--- flutter_application_1/lib/routes/route_helper.dart
+++ flutter_application_1/lib/routes/route_helper.dart
@@ -1,5 +1,8 @@
 import 'package:flutter_application_1/auth/sign_in_page.dart';
 import 'package:flutter_application_1/auth/sign_up_page.dart';
+import 'package:flutter_application_1/pages/food/popular_food_detail.dart';
+import 'package:flutter_application_1/pages/food/recommended_food_detail.dart';
+import 'package:flutter_application_1/pages/home/home_page.dart';
 import 'package:flutter_application_1/pages/home/main_food_page.dart';
 import 'package:flutter_application_1/pages/splash/splash_page.dart';
 import 'package:get/get.dart';
@@ -7,18 +10,24 @@ import 'package:get/get.dart';
 class RouteHelper {
   static const String initial = "/";
   static const String splashPage = "/splash-page";
+  static const String popularFood = "/popular-food";
+  static const String recommendedFood = "/recommended-food";
   static const String signIn = "/sign-in";
 
   static String getSplashPage() => '$splashPage';
   static String getInitial() => '$initial';
   static String getSignInPage() => '$signIn';
+  static String getPopularFood(int pageId) => '$popularFood?pageId=$pageId';
+  static String getRecommendedFood(int pageId) =>
+      '$recommendedFood?pageid=$pageId';
 
   static List<GetPage> routes = [
     GetPage(name: splashPage, page: () => SplashScreen()),
     GetPage(
         name: initial,
         page: () {
-          return SignUpPage();
+          //return signuppage for register
+          return HomePage();
         }),
     GetPage(
         name: signIn,
@@ -26,6 +35,23 @@ class RouteHelper {
           return SignInPage();
         },
         transition: Transition.fade),
+    GetPage(
+        name: popularFood,
+        page: () {
+          var pageId = Get.parameters['pageId'];
+          return PopularFoodDetail(
+            pageId: int.parse(pageId!),
+          );
+        },
+        transition: Transition.fadeIn),
+    GetPage(
+        name: recommendedFood,
+        page: () {
+          var pageId = Get.parameters['pageId'];
+          return RecommendedFoodDetail(
+            pageId: int.parse(pageId!),
+          );
+        },
+        transition: Transition.fadeIn),
   ];
-
 }
