7bd7d5a399c7ac791e782ab9376599aad66caba5
Nomikuza <uzumakisujai@gmail.com>
2024-04-01 20:36:48 +0700

    perf: adding app_constants instance for couple address to make dynamic code

--- flutter_application_1/lib/utils/app_constants.dart
+++ flutter_application_1/lib/utils/app_constants.dart
@@ -2,12 +2,20 @@ class AppConstants {
   static const String APP_NAME = "DBFood";
   static const int APP_VERSION = 1;
 
-  static const String BASE_URL = "http://localhost:8000";
+//dependencies
+  static const String BASE_URL = "https://mvs.bslmeiyu.com";
+  // static const String BASE_URL = "http://localhost:8000";
+  static const String BASE_URL_MEIYI = "https://mvs.bslmeiyu.com";
   static const String POPULAR_PRODUCT_URI = "/api/v1/products/popular";
   static const String RECOMMENDED_PRODUCT_URI = "/api/v1/products/recommended";
+  static const String UPLOAD_URL = '/uploads/';
 
   //auth end points
   static const String REGISTRATION_URI = "/api/register";
+  static const String LOGIN_URI = "/api/login";
 
   static const String TOKEN = "DBtoken";
+  static const String PHONE = "";
+  static const String PASSWORD = "";
+  static const String CART_LIST = "cart-list";
 }
