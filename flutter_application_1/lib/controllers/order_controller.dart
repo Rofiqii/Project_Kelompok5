02a5c4a76236f4eff59b32be28cbc893e02cf4bc
Rofiqi <e41222771@student.polije.ac.id>
2024-05-07 19:27:06 +0700

    update : order controller

--- flutter_application_1/lib/controllers/order_controller.dart
+++ flutter_application_1/lib/controllers/order_controller.dart
@@ -1,5 +1,18 @@
+import 'package:flutter_application_1/data/repository/order_rapo.dart';
 import 'package:get/get.dart';
 
 class OrderController extends GetxController implements GetxService{
-  
+  OrderRepo orderRepo;
+  OrderController({required this.orderRepo});
+  bool _isLoading=false;
+  bool get isLoading=>_isLoading;
+  Future<void> placeOrder()async{
+    _isLoading=true;
+    Response response = await orderRepo.placeOrder();
+    if(response.statusCode==200){
+      _isLoading=false;
+      String string= response.body["massage"];
+      String orderID= response.body["order_id"].toString();
+    }
+  }
 }
